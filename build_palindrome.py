b26ca55c0f7fcab18c1899fea78de45f4819e7b5
Konstantin Kononenko <kalikrit@gmail.com>
2017-04-12 17:00:51 +0300

    build_palindrom

---
+++ build_palindrome.py
@@ -0,0 +1,51 @@
+"""
+Task
+    Given a string str, find the shortest possible string which can be achieved
+    by adding characters to the end of initial string to make it a palindrome.
+
+Example
+    For str = "abcdc", the output should be "abcdcba".
+
+Input/Output
+    [input] string str
+    A string consisting of lowercase latin letters.
+
+Constraints: 3 ≤ str.length ≤ 10.
+[output] a string
+"""
+"""
+# my solution
+
+def is_palindrome(string):
+    '''
+    checks whether the given string is a palindrome
+    '''
+    return string == string[::-1]
+
+
+def build_palindrome(string):
+    '''
+    makes palindrom out of the given string
+    '''
+    if is_palindrome(string):
+        return string
+    else:
+        i = 0
+        for _ in string:
+            new_str = string + string[i::-1]
+            if is_palindrome(new_str):
+                return new_str
+            i += 1
+"""
+
+
+def build_palindrome(str):
+    """
+    best practices
+    """
+    suf = ""
+    for c in str:
+        pal = str + suf
+        if pal == pal[::-1]:
+            return pal
+        suf = c + suf
